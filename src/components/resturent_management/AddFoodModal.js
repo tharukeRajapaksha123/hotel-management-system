a93d57c380b5c862a2b64289f407629cc2727182
tharukerajapaksha123 <darksparrow156@gmail.com>
2022-10-24 12:04:44 +0530

    ADD:food-items crud done

--- src/components/resturent_management/AddFoodModal.js
+++ src/components/resturent_management/AddFoodModal.js
@@ -1,8 +1,154 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Modal, Form, Input, Row, Col, Button, Dropdown, Menu } from "antd"
+import axios from "axios"
+import { baseUrl } from "../../config"
 
-const AddFoodModal = () => {
+const AddFoodModal = ({ handleOk, handleCancel, isModalOpen, foodData }) => {
+
+  const [form] = Form.useForm();
+
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [availability, setAvailability] = useState(1);
+  const [imageUrl, setImageUrl] = useState("");
+
+  const [requiredMark, setRequiredMarkType] = useState('optional');
+  const onRequiredTypeChange = ({ requiredMarkValue }) => {
+    setRequiredMarkType(requiredMarkValue);
+  };
+
+
+
+
+  const menu = (
+    <Menu
+      items={[
+        {
+          key: '1',
+          label: (
+            <p onClick={()=>{setAvailability(1)}}>
+              In Stock
+            </p>
+          ),
+        },
+        {
+          key: '2',
+          label: (
+            <p onClick={()=>{setAvailability(0)}} >
+              Out Of Stock
+            </p>
+          ),
+        },
+        
+      ]}
+    />
+  );
+
+
+  useEffect(() => {
+
+    if (foodData != null) {
+      console.log("called");
+      setName(foodData["name"]);
+      setPrice(foodData["price"])
+
+      setImageUrl(foodData["image"])
+      setAvailability(foodData["available"])
+    }
+  }, [foodData]);
+
+
+  const createFood = async () => {
+    const room = {
+      "image": imageUrl,
+      "name": name,
+      "price": price,
+      "available": availability,
+    }
+
+    if (foodData) {
+      await axios.put(`${baseUrl}/food-controller/update-food-item/${foodData["_id"]}`, room)
+    } else {
+      await axios.post(`${baseUrl}/food-controller/create-food-item/`, room)
+    }
+    handleOk();
+  }
+
   return (
-    <div>AddFoodModal</div>
+    <Modal title={foodData ? "Edit Food Item" : "Add Food Item"} open={isModalOpen} footer={null} width={1000}>
+      <Form
+        form={form}
+        layout="vertical"
+        initialValues={{
+          requiredMarkValue: requiredMark,
+        }}
+        onValuesChange={onRequiredTypeChange}
+        requiredMark={requiredMark}
+        action={() => { createFood() }}
+      >
+        <Row>
+          <Col span={7}>
+            <Form.Item label="Food name" required tooltip="Food name is required">
+              <Input placeholder="" value={name} onChange={(e) => { setName(e.target.value) }} />
+            </Form.Item>
+          </Col>
+          <Col span={1} />
+          <Col span={7}>
+            <Form.Item
+              required
+              label="Price"
+              tooltip={{
+                title: 'Price for food item is required',
+              }}
+            >
+              <Input placeholder="" value={price} onChange={(e) => { setPrice(e.target.value) }} />
+            </Form.Item>
+          </Col>
+          <Col span={1} />
+          <Col span={7}>
+            <Form.Item
+              required
+              label="Availability"
+              tooltip={{
+                title: 'Price for food item is required',
+              }}
+            >
+              <Dropdown overlay={menu} placement="bottom" arrow >
+                <Button style = {{width : "100%"}}>{availability === 1 ? "In Stock" : "Out of Stock"}</Button >
+              </Dropdown>
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+
+          <Col span={24}>
+            <Form.Item
+              required
+              label="Image url"
+              tooltip={{
+                title: 'Food image is required',
+              }}
+            >
+              <Input placeholder="" value={imageUrl} onChange={(e) => { setImageUrl(e.target.value) }} />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col span={12} />
+          <Col span={4} />
+          <Col span={4}>
+            <Button onClick={() => { handleCancel() }} style={{ width: "100%", margin: "0 8px" }}>Cancel</Button>
+          </Col>
+          <Col span={4}>
+            <Form.Item>
+              <Button type="primary" htmlType='submit' onClick={() => { createFood(); }} style={{ width: "100%", margin: "0 8px" }}>{foodData ? "Edit" : "Submit"}</Button>
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+    </Modal>
   )
 }
 
--- src/components/resturent_management/FoodItemsBody.js
+++ src/components/resturent_management/FoodItemsBody.js
@@ -9,89 +9,119 @@ import { baseUrl } from '../../config'
 import axios from "axios"
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 import DeleteModal from '../../conmmon/DeleteModal'
+import AddFoodModal from './AddFoodModal'
 
 const FoodItemsBody = () => {
-  const [foods, setFoods] = useState([])
+   const [foods, setFoods] = useState([])
+   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false)
+   const [selectedFood, setSelectedFood] = useState(null)
+   const [isModalOpen, setIsModalOpen] = useState(false)
+   const [isEditModalOpen,setEditModalOpen] = useState(false);
 
 
-  const showAddModal = () => {
+   const showAddModal = () => {
+      setIsModalOpen(true)
+   }
 
-  }
+   const handleOk = async () => {
+      await refresher()
+      setIsModalOpen(false)
+   }
 
-  const setSelectedFood = () => {
-
-  }
-
-  const openCloseEditMdal = () => {
-  }
-
-  const setIsDeleteModalOpen = () => { }
-
-  const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name'
-    },
-    {
-      title: 'Price',
-      dataIndex: 'price',
-      key: 'price',
-    },
-    {
-      title: 'Available',
-      dataIndex: 'available',
-      key: 'available',
-    },
-    {
-      title: 'Image',
-      key: 'image',
-      render: (_, record) => (
-        <Space size="middle">
-          <img src={`${record["image"]}`} alt="room" width={50} height={50} />
-        </Space>
-      ),
-    },
-    {
-      title: 'Action',
-      key: 'action',
-      render: (_, record) => (
-        <Space size="middle">
-          <Button icon={<EditOutlined />} onClick={() => { setSelectedFood(record); openCloseEditMdal(); }}></Button>
-          <Button icon={<DeleteOutlined />} onClick={() => { setSelectedFood(record); setIsDeleteModalOpen(true); }}></Button>
-        </Space>
-      ),
-    },
-  ];
+   const handleCancel = () => {
+      setIsModalOpen(false)
+   }
 
 
-  const refresher = () => {
-    let f = []
-    axios.get(`${baseUrl}/food-controller`).then(resul => {
-      f = resul.data["Food_Items"]
-      setFoods([...f])
-    }).catch(err => console.log(`error ${err}`))
-  }
+   const openCloseEditMdal = async() => {
+      await refresher();
+      setEditModalOpen(!isEditModalOpen)
+   }
 
-  useEffect(() => {
-    let f = []
-    axios.get(`${baseUrl}/food-controller`).then(resul => {
-      f = resul.data["Food_Items"]
-      setFoods([...f])
-    }).catch(err => console.log(`error ${err}`))
-  }, [])
 
-  return (
-    <Container style={{ padding: "16px" }}>
-      <CustomColumn>
-        <CustomRow style={{ justifyContent: "space-between", height: "50px" }}>
-          <h1>Resturent Management</h1>
-          <AddButton onclick={showAddModal} />
-        </CustomRow>
-        <Table columns={columns} dataSource={foods} style={{ width: "100%", flex: 1 }} />
-      </CustomColumn>
-    </Container>
-  )
+   const deleteModalCancel = () => {
+      setIsDeleteModalOpen(false)
+   }
+
+
+
+   const deleteModalHandleOk = async () => {
+      await axios.delete(`${baseUrl}/food-controller/delete-food-item/${selectedFood["_id"]}`);
+      await refresher();
+      setIsDeleteModalOpen(false)
+
+
+   }
+
+
+   const columns = [
+      {
+         title: 'Name',
+         dataIndex: 'name',
+         key: 'name'
+      },
+      {
+         title: 'Price',
+         dataIndex: 'price',
+         key: 'price',
+      },
+      {
+         title: 'Available',
+         dataIndex: 'available',
+         key: 'available',
+      },
+      {
+         title: 'Image',
+         key: 'image',
+         render: (_, record) => (
+            <Space size="middle">
+               <img src={`${record["image"]}`} alt="room" width={50} height={50} />
+            </Space>
+         ),
+      },
+      {
+         title: 'Action',
+         key: 'action',
+         render: (_, record) => (
+            <Space size="middle">
+               <Button icon={<EditOutlined />} onClick={() => { setSelectedFood(record); openCloseEditMdal(); }}></Button>
+               <Button icon={<DeleteOutlined />} onClick={() => { setSelectedFood(record); setIsDeleteModalOpen(true); }}></Button>
+            </Space>
+         ),
+      },
+   ];
+
+
+   const refresher = async () => {
+      let f = []
+      await axios.get(`${baseUrl}/food-controller`).then(resul => {
+         f = resul.data["Food_Items"]
+         setFoods([...f])
+      }).catch(err => console.log(`error ${err}`))
+   }
+
+   useEffect(() => {
+      let f = []
+      axios.get(`${baseUrl}/food-controller`).then(resul => {
+         f = resul.data["Food_Items"]
+         setFoods([...f])
+      }).catch(err => console.log(`error ${err}`))
+   }, [])
+
+   return (
+      <Container style={{ padding: "16px" }}>
+         <CustomColumn>
+            <CustomRow style={{ justifyContent: "space-between", height: "50px" }}>
+               <h1>Resturent Management</h1>
+               <AddButton onclick={showAddModal} />
+            </CustomRow>
+            <Table columns={columns} dataSource={foods} style={{ width: "100%", flex: 1 }} />
+         </CustomColumn>
+         <AddFoodModal handleOk={handleOk} handleCancel={handleCancel} isModalOpen={isModalOpen} />
+         <AddFoodModal handleOk={openCloseEditMdal} handleCancel={openCloseEditMdal} isModalOpen={isEditModalOpen} foodData={selectedFood} />
+         <DeleteModal text={"Delete room"} handleCancel={deleteModalCancel} handleOk={deleteModalHandleOk} isModalOpen={isDeleteModalOpen} />
+      </Container>
+   )
 }
 
 export default FoodItemsBody
